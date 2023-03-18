1af074ffb8921fb8f3d13199763225f42c44eb05
Emmanuel-urama <emmanuel.uramag@gmail.com>
2023-03-17 22:44:02 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-this program prints the alphabets
+ *
+ * Return:this returns zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
