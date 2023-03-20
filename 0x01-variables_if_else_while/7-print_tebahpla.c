0508afba14b2c3ecd4ac548ae33de08dfdfb19ea
Emmanuel-urama <emmanuel.uramag@gmail.com>
2023-03-20 10:04:57 -0700

    task 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-print lower case alphabet
+ *
+ * Return:Always Zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i <= 'a'; i--)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
