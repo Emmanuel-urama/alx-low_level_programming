4289b126a651bbfaef0138e410fc055a94d67cda
Emmanuel-urama <emmanuel.uramag@gmail.com>
2023-03-22 21:37:43 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints alphabet in lower cases
+ *
+ * Returns: Zero
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
