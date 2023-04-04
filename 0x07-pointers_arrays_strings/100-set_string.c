3604a7e6df86887a0bea37c2343fbea98d9bd1a3
Emmanuel-urama <emmanuel.uramag@gmail.com>
2023-04-03 21:04:38 -0700

    Task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to string
+ * @s: pointer to pointer
+ * @to: pointer to string
+ * Return: Always zero
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
